20d85530e3c99a8c9f792fe8855eb774b723021a
Hashim Raza <108229556+HashimRaza11@users.noreply.github.com>
2023-12-29 20:05:07 +0000

    container config done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as a newer version
+
+//alert("hello") we r using node js not browser
+
+let name ="Hashim"
+let age= 25
+let isLoggedIn = false
+let state = null 
+
+// numebr => 2 to power 53
+//bigint
+// string => ""
+// boolean +> true/false
+//null => standalone value 
+// undefined =>
+//symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
