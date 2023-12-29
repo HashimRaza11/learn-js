b8606d9835429e4dc98f00599d617fd04ef1ca07
Hashim Raza <108229556+HashimRaza11@users.noreply.github.com>
2023-12-29 19:42:54 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 178687
+let accountEmail = "raza.hashim93@gmail.com"
+var accountPasword = "12345 "
+accountCity ="Azamgarh"
+let accountState;
+
+accountEmail = "hashim@gmail.com"
+accountPasword = 883754
+accountCity = "Lucknow"
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPasword,accountCity,accountState]);
