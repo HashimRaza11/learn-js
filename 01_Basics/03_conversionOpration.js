2202ba9136e4f29b172b6e97186ea1f5b6878b33
Hashim Raza <108229556+HashimRaza11@users.noreply.github.com>
2023-12-30 12:26:53 +0000

    container config done

---
+++ 01_Basics/03_conversionOpration.js
@@ -0,0 +1,24 @@
+let score ="33abc"
+//true => 1; false => 0
+
+console.log(typeof score);
+console.log(typeof(score));
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+//0=>false
+//"" =>false
+// "Hashim" =>true
+
+let someNumber = 33
+ 
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
